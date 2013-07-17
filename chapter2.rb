549bcab1e41750f1687a2b59cf02df749b0e6815
Jordan Day <jordan.day@perceptivesoftware.com>
2013-07-17 16:33:34 -0500

    examples through page 46

--- chapter2.rb
+++ chapter2.rb
@@ -426,3 +426,105 @@ Machine.new(
   ),
   { :x => Number.new(1)} 
 ).run
+
+# Big-step semantics
+class Number
+  def evaluate(environment)
+    self
+  end
+end
+
+class Boolean
+  def evaluate(environment)
+    self
+  end
+end
+
+class Variable
+  def evaluate(environment)
+    environment[name]
+  end
+end
+
+class Add
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value + right.evaluate(environment).value)
+  end
+end
+
+class Multiply
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value * right.evaluate(environment).value)
+  end
+end
+
+class LessThan
+  def evaluate(environment)
+    Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
+  end
+end
+
+Number.new(23).evaluate({})
+
+Variable.new(:x).evaluate({ :x => Number.new(23) })
+
+LessThan.new(
+  Add.new(Variable.new(:x), Number.new(2)),
+  Variable.new(:y)
+).evaluate({ :x => Number.new(2), :y => Number.new(5) })
+
+class Assign
+  def evaluate(environment)
+    environment.merge({ name => expression.evaluate(environment) })
+  end
+end
+
+class DoNothing
+  def evaluate(environment)
+    environment
+  end
+end
+
+class If
+  def evaluate(environment)
+    case condition.evaluate(environment)
+    when Boolean.new(true)
+      consequence.evaluate(environment)
+    when Boolean.new(false)
+      alternative.evaluate(environment)
+    end
+  end
+end
+
+class Sequence
+  def evaluate(environment)
+    second.evaluate(first.evaluate(environment))
+  end
+end
+
+statement = 
+  Sequence.new(
+    Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
+    Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
+  )
+
+statement.evaluate({})
+
+class While
+  def evaluate(environment)
+    case condition.evaluate(environment)
+    when Boolean.new(true)
+      evaluate(body.evaluate(environment))
+    when Boolean.new(false)
+      environment
+    end
+  end
+end
+
+statement =
+  While.new(
+    LessThan.new(Variable.new(:x), Number.new(5)),
+    Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
+  )
+
+statement.evaluate({ :x => Number.new(1) })
